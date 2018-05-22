7b9e6460ba6d1d54cad206af96f4ef744f52843f
Unknown <victorgesuato3@gmail.com>
2018-05-22 14:07:10 -0400

    NumberFragment

---
+++ app/src/main/java/com/midterm/lasalle/meuexemploswipe1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.midterm.lasalle.meuexemploswipe1;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SwipeAdapter swipeAdapter = new SwipeAdapter(getSupportFragmentManager());
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(swipeAdapter);
+    }
+}
---
+++ app/src/main/java/com/midterm/lasalle/meuexemploswipe1/SwipeAdapter.java
@@ -0,0 +1,29 @@
+package com.midterm.lasalle.meuexemploswipe1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class SwipeAdapter extends FragmentStatePagerAdapter {
+
+    public SwipeAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = new NumberFragment();
+        Bundle args = new Bundle();
+        args.putInt("number",position + 1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+
+}
